4256f5302b3915ba5300e7f25a343d62e02de1ea
unknown <bibhudattarw@gmail.com>
2025-01-03 22:32:35 +0530

    updated changesdfddd

--- Backend/index.js
+++ Backend/index.js
@@ -22,12 +22,12 @@ const cors = require("cors");
 dbConnect();
 app.use(morgan("dev"));
 const corsOptions = {
-  origin: ['https://romaadmin.expsolutions.net', 'https://roma.expsolutions.net'], // Array of allowed origins
+  origin: ['https://romaadmin.expsolutions.net', 'https://roma.expsolutions.net','http://localhost:3000'], // Array of allowed origins
   methods: 'GET,POST,PUT,DELETE', // Allow specific HTTP methods
   allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
